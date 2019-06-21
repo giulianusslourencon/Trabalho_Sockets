89629ae2945e5be469d77cbc4f0011c43057a779
Psy <giulianolourencon@gmail.com>
2019-06-20 21:06:12 -0300

    Created simple server

---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+
+ip = "0.0.0.0"
+porta = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, porta))
+server.listen(9)
+
+print "Ouvindo em %s:%d" % (ip, porta)
+
+while True:
+    client, addr = server.accept()
+    print "Conexão recebida por %s:%d" % (addr[0], addr[1])
+
+    cliente = threading.Thread(target=cliente, args=(client,))
+    cliente.start()
+
+--------------------------------------------------------------------------
+
+def cliente(cliente_socket):
+    resposta = cliente_socket.recv(1024)
+    print "Resposta = %s" % resposta
+
+    cliente_socket.send("TA CHOVENDO AI??")
+    cliente_socket.close()
