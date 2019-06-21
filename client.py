4797dd8c717b0e10ae686b63700a205c83b5f277
Psy <giulianolourencon@gmail.com>
2019-06-21 10:03:31 -0300

    Chat client implemented and working

--- client.py
+++ client.py
@@ -1,23 +1,51 @@
 import socket
+import select
+import errno
+import sys
 
-HEADERSIZE = 10
+########## CONSTANTS ##########
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 9999))
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 9999
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode("utf-8")
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header+username)
 
 while True:
-    full_msg = ''
-    new_msg = True
-    while True:
-        msg = s.recv(16)
-        if new_msg:
-            print(f"Tamanho da mensagem: {msg[:HEADERSIZE]}")
-            msg_len = int(msg[:HEADERSIZE])
-            new_msg = False
+    message = input(f"{my_username} > ")
 
-        full_msg += msg.decode("utf-8")
-        if len(full_msg)-HEADERSIZE == msg_len:
-            print("Recebeu tudo")
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ''
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("Conexao encerrada pelo servidor")
+                sys.exit()
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Erro de leitura', str(e))
+        continue
+
+    except Exception as e:
+        print('Erro', str(e))
+        sys.exit()
