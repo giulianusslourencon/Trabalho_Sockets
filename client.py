1e4b1ec960f586b46af06a2084439686d0a0d3e1
Psy <giulianolourencon@gmail.com>
2019-06-21 08:48:25 -0300

    Agora as mensagens sao tratadas com um header de tamanho

--- client.py
+++ client.py
@@ -1,12 +1,23 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 9999))
 
-full_msg = ""
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"Tamanho da mensagem: {msg[:HEADERSIZE]}")
+            msg_len = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+        if len(full_msg)-HEADERSIZE == msg_len:
+            print("Recebeu tudo")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 9999))
 s.listen(5)
@@ -7,5 +9,8 @@ s.listen(5)
 while True:
     clientSocket, address = s.accept()
     print(f"Conexão recebida por {address}")
-    clientSocket.send(bytes("Aehoooo ta conectado agr irmao, sua alma eh minha heuheue", "utf-8"))
-    clientSocket.close()
+
+    msg = "Aehoooo ta conectado agr irmao, sua alma eh minha heuheue"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientSocket.send(bytes(msg, "utf-8"))
